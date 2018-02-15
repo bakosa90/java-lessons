d53080a8aad7b8ba1e89a8d624e977d4cb76a5ee
Sveta <bakosa90@gmail.com>
2018-02-15 20:59:14 +0300

    initial commit

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a=5;
+	int b=7;
+	int c=(a*b+2)-3;
+	System.out.println(c);
+	int d=a/b;
+	System.out.println(d);
+	float f=20.f/8f;
+	System.out.println(f);
+    }
+}
