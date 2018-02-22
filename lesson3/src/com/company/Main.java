ce08ba9f006f9d0beac2e72ac9c6a9a5ba1322f5
Sveta <bakosa90@gmail.com>
2018-02-22 20:39:20 +0300

    lesson2

---
+++ lesson3/src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Чтение данных
+       /* Scanner s=new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year=s.nextInt();
+        System.out.print("Год:");
+        if ((year%4==0&&year%100!=0)||year%400==0){
+            System.out.println("Високосный!");
+        }
+        else {
+            System.out.println("Не високосный!");*/
+
+       /*float ages[]=new float[5];
+       ages[0]=13.5f;
+       ages[1]=12.3f;
+       ages[2]=22f;
+       ages[3]=46.8f;
+       ages[4]=9.2f;
+      // float avg=(ages[0]+ages[1]+ages[2]+ages[3]+ages[4])/5;
+
+       float sum=0;
+       for(int i=0;i<ages.length;i=i+1){
+           sum=sum+ages[i];
+       }
+       float avg=sum/ages.length;
+        System.out.println(avg);
+       String fruits[]={
+               "apple","orange","banana","grapes"
+       };
+        for(int i=0;i<fruits.length;i=i+1){
+            System.out.println(fruits[i]);
+        }*/
+       /*class com.company.User {
+           int age;
+           String name;
+           boolean male;
+           void say(){
+               System.out.println("Hello!");
+           }
+       }
+
+       User u1=new User();
+       u1.age=20;
+       u1.name="Petya";
+       u1.male=true;
+       u1.say();
+       u1.Whatisyourname();
+       User u2=new User();
+       u2.name="Sveta";
+       u2.male=false;
+       u2.Whatisyourname();*/
+
+       String s;
+       s.
+
+
+                                     }
+          }
+
---
+++ lesson3/src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class User {
+    int age;
+    String name;
+    boolean male;
+    void say() {
+        System.out.println("Hello!");
+    }
+
+    void Whatisyourname() {
+            System.out.println("Mu name is"+name);
+    }
+}
